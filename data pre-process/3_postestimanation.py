d2977efdbfdac874d32d3e40121af90ce771c29e
s1100611 <s1100611@users.noreply.github.com>
2025-07-31 14:19:34 +0800

    data pre process

--- data pre-process/3_postestimanation.py
+++ data pre-process/3_postestimanation.py
@@ -23,22 +23,21 @@ for x in dirs:
 os.makedirs(dir_path + 'dataset/marked/' + folder, exist_ok=True)
 
 for name in dirs:
-    #导入模型
     mp_pose=mp.solutions.pose
     mp_drawing=mp.solutions.drawing_utils
-    pose=mp_pose.Pose(static_image_mode=True,#选择静态图片还是连续视频帧
-                 model_complexity=2,#选择人体姿态关键点检测模型，0性能差但快，2性能好但慢，1介于之间
-                 smooth_landmarks=True,#是否选择平滑关键点
-                 min_detection_confidence=0.5,#置信度阈值
-                 min_tracking_confidence=0.5)#追踪阈值
+    pose=mp_pose.Pose(static_image_mode=True,#選擇圖片或是影片
+                 model_complexity=2,#選擇人體姿態關節點檢測模型，0性能差但快，2性能好但慢，1介於之間
+                 smooth_landmarks=True,#是否選擇平滑關節點
+                 min_detection_confidence=0.5,#系統判定姿勢偵測成功的最低可信度分數，範圍是[0,1]
+                 min_tracking_confidence=0.5)#系統判定姿勢追蹤成功的最低可信度分數
 
 
     img = cv2.imread(dir_path + folder + '/' + name + '.jpg')
-    #查看读入的图像
+    #查看讀入的圖像
 
-    #BGR转RGB
+    #BGR轉RGB
     img_RGB=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    #输入模型，获取预测结果
+    #輸入模型，獲取預測结果
     results=pose.process(img_RGB)
     mp_drawing.draw_landmarks(img,results.pose_landmarks,mp_pose.POSE_CONNECTIONS)
     
